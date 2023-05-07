dfdce2010ad31038818a2b0e2b1d40b8611e0045
kochan4php <aprodeosubarno@gmail.com>
2023-05-08 04:33:59 +0700

    add root route

--- src/api.js
+++ src/api.js
@@ -2,6 +2,33 @@ const bookHandler = require('./bookHandler');
 
 module.exports = [
     {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            const response = h.response({
+                message:
+                    "Welcome to NodeJS Bookshelf Submission Backend API with Hapi Framework! Access '/books' path to do CRUD bookshelf operation."
+            });
+
+            response.code(200);
+            response.type('application/json');
+            return response;
+        }
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            const response = h.response({
+                message: `Cannot access this path using ${request.method} method`
+            });
+
+            response.code(400);
+            response.type('application/json');
+            return response;
+        }
+    },
+    {
         method: 'POST',
         path: '/books',
         handler: bookHandler.addBook
